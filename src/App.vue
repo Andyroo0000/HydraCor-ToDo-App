5cdf79894affcb3560f4a73537d2fe5c9f112fbb
Andrew Leung <andrewleung@admins-MacBook-Pro-2.local>
2026-07-16 11:29:11 -0400

    Initial Project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
-const msg = 'Todo App';
+import { ref } from 'vue'
+
+const count = ref(1)
+
+function increament() {
+  count.value++
+}
 </script>
 
 <template>
-  <h1 style="text-align: center;">
-    {{ msg }}
+  <h1 style="text-align: center; color:white; size: 1000px;">
+    Andrew's Todo App
   </h1>
+  <h2 style="text-align: left; padding-left: 50px">
+    Add
+  </h2>
+  <!-- <button @click="increament">INcrement Count</button> -->
 </template>
-
