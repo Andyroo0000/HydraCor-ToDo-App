7bfac0100e83d16bff000498a3331fb2f51ec79f
Andyroo0000 <andrew-leung@comcast.net>
2026-07-22 16:01:18 -0400

    Update listToDo.vue

--- src/components/listToDo.vue
+++ src/components/listToDo.vue
@@ -1,14 +1,29 @@
 <script setup>
+import changeStatusToDo from './changeStatusToDo.vue';
+import deleteToDo from './deleteToDo.vue';
+import { ref } from 'vue'
+import Checkbox from './checkbox.vue';
+import panelToDo from './panelToDo.vue';
+
 const userData = defineProps({
-    todos: Array    
+    todos: Array
 })
+
+
+
 </script>
 
 <template>
     
-    <ul style="list-style: none">
-        <li v-for="user_action in userData.todos" :key="user_action" class="text-black text-center">
-            <p class="text-black">{{ user_action.task }} : {{user_action.completion}}</p>
+    <ul>
+        <li v-for="userAction in userData.todos" :key="userAction" class="text-black text-center">
+            <div class="flex justify-center gap-2">
+                <input v-model="userAction.selected" type="checkbox" class="accent-black">
+                <p class="text-black">{{ userAction.task }}</p>
+            </div>
+            <panelToDo :todos="userData.todos" :task="userAction" v-if="userAction.selected"></panelToDo>
         </li>
     </ul>
+
+
 </template>
