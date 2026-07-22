b7ea7a3a0991117b6e82f2bfc6394ef0a68b82ed
Andyroo0000 <andrew-leung@comcast.net>
2026-07-22 16:01:48 -0400

    Create addNotesToDo.vue

---
+++ src/components/addNotesToDo.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const userData = defineProps({
+        todos: Array
+    })
+
+</script>
+
+
+<template>
+    <form @submit.prevent="addTodo">
+        <input v-model="userTask" type="text" class="bg-blue-500 hover:bg-blue-700 text-white rounded" placeholder="Enter a ToDo">
+        <input v-model="completionStatus" type="text" class="bg-blue-500 hover:bg-blue-700 text-white rounded" placeholder="Enter a ToDo">
+        <Button @click="addUserData">Add to List</Button>
+
+    </form>
+
+</template>
