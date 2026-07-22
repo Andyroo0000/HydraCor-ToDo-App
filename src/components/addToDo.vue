cb1ed49bd26c22cdb5cf282492dc271f62c7eb69
Andyroo0000 <andrew-leung@comcast.net>
2026-07-22 16:00:52 -0400

    Update addToDo.vue

--- src/components/addToDo.vue
+++ src/components/addToDo.vue
@@ -6,18 +6,31 @@ const userData = defineProps({
     todos: Array    
 })
 
+const currentTime = new Date().toLocaleString()
+
 const userTask = ref('')
 
+const userNotes = ref('')
+
 const completionStatus = ref('')
+
+const targetDate = ref('')
 
 function addUserData() {
         userData.todos.push({
           task: userTask.value,
           completion: completionStatus.value,
+          selected: false,
+          timeCreated: currentTime,
+          endTme: currentTime,
+          targetTime: targetDate,
+          notes: userNotes.value,
+
   })
 
   userTask.value = ""
   completionStatus.value = ""
+  userNotes.value = ""
 }
 
  </script>
@@ -26,7 +39,15 @@ function addUserData() {
 <template>
     <form @submit.prevent="addTodo">
         <input v-model="userTask" type="text" class="bg-blue-500 hover:bg-blue-700 text-white rounded" placeholder="Enter a ToDo">
-        <input v-model="completionStatus" type="text" class="bg-blue-500 hover:bg-blue-700 text-white rounded" placeholder="Enter a ToDo">
+        <select v-model="completionStatus" placeholder="Status">
+            <option disabled value="">Select Status</option>
+            <option value="Incomplete">Not Done</option>
+            <option value="Complete">Done</option>
+            <option value="blocked">Blocked</option>
+        </select>
+        <input v-model="targetDate" type="date" class="bg-blue-500 hover:bg-blue-700 text-white rounded" placeholder="Enter Notes">
+        
+        <input v-model="userNotes" type="text" class="bg-blue-500 hover:bg-blue-700 text-white rounded" placeholder="Enter Notes">
         <Button @click="addUserData">Add to List</Button>
 
     </form>
