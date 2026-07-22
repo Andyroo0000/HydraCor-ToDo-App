307b089c1be33319479c31dc8da8520e1dbf0ddb
Andyroo0000 <andrew-leung@comcast.net>
2026-07-22 16:02:36 -0400

    Create deleteToDo.vue

---
+++ src/components/deleteToDo.vue
@@ -0,0 +1,31 @@
+<script setup>
+import DeleteButton from './deleteButton.vue';
+import { ref } from "vue"
+
+const userTask = ref()
+
+const userData = defineProps({
+    todos: Array,
+    task: Array
+})
+
+function deleteUserData() {
+    const index = userData.todos.findIndex(todo => todo.task === userData.task.task)
+
+    if (index !== -1) {
+        userData.todos.splice(index, 1)
+    }
+    else {
+        return
+    }
+    userTask.value = ""
+
+}
+
+</script>
+
+<template>
+    <DeleteButton @click="deleteUserData">Delete</DeleteButton>
+
+
+</template>
