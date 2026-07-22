cb8e090640622a8d5ef74732f36fc063f52b2c6f
Andyroo0000 <andrew-leung@comcast.net>
2026-07-22 16:02:52 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import DeleteButton from './components/deleteButton.vue';
 import { ref, reactive, watch } from 'vue'
 import PrintList from './components/listToDo.vue'
 import AddToDo from './components/addToDo.vue'
+import DeleteToDo from './components/deleteToDo.vue';
+import Checkbox from './components/checkbox.vue';
 
   const userData = reactive([])
 
@@ -13,21 +15,23 @@ import AddToDo from './components/addToDo.vue'
 </script>
 
 <template>
+  
+  <div class="bg-gray-100 min-h-screen">
+    <div class="bg-white rounded-2xl shadow-xl">
+  `   <div class="bg-gray-100 flex items-top justify-center pt-8">
+      <h1 class="text-blue-600 text-4xl font-bold">
+          Andrew's ToDo App
+      </h1>
+      </div>
 
-  <AddToDo :todos="userData"></AddToDo>
+      <div class="flex justify-center pt-8">
+        <Button>Click Here</Button>
+        <DeleteButton>Click Here</DeleteButton>
+      </div>
 
-  <div class="bg-white flex items-top justify-center pt-8">
-    <h1 class="text-5xl font-bold text-blue-500">
-      Andrew's ToDo App
-    </h1>
+      <AddToDo :todos="userData"></AddToDo>
+      <DeleteToDo :todos="userData"></DeleteToDo>
+      <PrintList :todos="userData"></PrintList>`
+    </div>
   </div>
-
-  <div class="flex justify-center pt-8">
-    <Button>Click Here</Button>
-    <DeleteButton>Click Here</DeleteButton>
-  </div>
-
-  <PrintList :todos="userData"></PrintList>
-
-
 </template>
